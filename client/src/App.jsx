15348ed8d583bb5031e7e58238fb3ed0686301ea
DANIGAIN <danigain1234@gmail.com>
2023-04-24 20:37:48 +0600

    done with login and sign up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import { useEffect, useState } from 'react';
+import { auth } from './firebase';
 
 function App() {
+   const [userName , setUserName] = useState('');
+
+
+   useEffect(()=>{
+      auth.onAuthStateChanged((user)=>
+      {
+         if(user)
+         {
+            setUserName(user.displayName);
+         }else 
+         {
+            setUserName('');
+         }
+      });
+
+   },[])
+
+
    return (
-    <div>app </div>
+    <div className='App'>
+      <BrowserRouter>
+      <Routes>
+         <Route path='/' element={<Home name ={userName} />}>Home</Route>
+         <Route path='/login' element={<Login/>}>Home</Route>
+         <Route path='/signup' element={<Signup/>}>Home</Route>
+      </Routes>
+      </BrowserRouter>
+
+    </div>
    )
 }
 
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+export default function Home(props)
+{
+    return (
+        <div>
+            <div>
+                <h1><Link to="/login">Login</Link></h1>
+                <h1><Link to="/signup">Sineup</Link></h1>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div>
+                <h2>{props.name ? `welcome to - ${props.name}`: 'please login '}</h2>
+            </div>
+        </div>
+           
+
+        )
+}
