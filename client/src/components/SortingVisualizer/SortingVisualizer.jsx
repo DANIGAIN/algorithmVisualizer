dec62b95f8480d5b21c8e25cd04416b727246fc2
DANIGAIN <danigain1234@gmail.com>
2023-04-25 21:16:32 +0600

    implement random array

--- client/src/components/SortingVisualizer/SortingVisualizer.jsx
+++ client/src/components/SortingVisualizer/SortingVisualizer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './SortingVisualizer.css';
 
 
@@ -10,27 +10,60 @@ let randomIntFromInterval = (min, max) => {
 
 export default function SortingVisualizer() {
 
-    const [array, setArray] = useState([]);
 
+    const [array, setArray] = useState([]);
 
     useEffect(() => {
 
-         for(let i = 0  ;i<100 ; i++)
-         {
-            setArray([...array,array.push(randomIntFromInterval(5,1000))]);
-         }
-       
+        let x = () => {
+            for (let i = 0; i < 100; i++) {
+                setArray([...array, array.push(randomIntFromInterval(5, 730))])
+            }
+        }
+        return x;
 
     }, []);
 
+    let mergeSort = () => { };
+    let bubbleSort = () => { };
+    let quickSort = () => { };
+    let heapSort = () => { };
+
+
+    let resetArray = () => {
+        let a = [];
+
+        for (let i = 0; i < 100; i++) {
+            a.push(randomIntFromInterval(5, 730));
+        }
+        return a;
+
+    }
+
+
     return (
         <>
-          {array.map((value ,index) =>
-          (
-               <div className="array-bar" key={index}> 
-                  {value}
-               </div>
-          ))}
+            <div className="array-container">
+                {array.map((value, index) =>
+                (
+                    <div
+                        className="array-bar"
+                        key={index}
+                        style={{ height: `${value}px` }}>
+
+
+                    </div>
+                ))}
+                <br />
+
+                <button onClick={() => setArray(resetArray)}> Generate New array </button>
+                <button onClick={() => mergeSort()}> mergeSort </button>
+                <button onClick={() => bubbleSort()}> bubbleSort</button>
+                <button onClick={() => quickSort()}>quickSort </button>
+                <button onClick={() => heapSort()}> heapSort </button>
+
+
+            </div>
         </>
     )
 }
