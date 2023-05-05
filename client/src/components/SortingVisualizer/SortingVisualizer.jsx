624334e673baf769ca1b17734db0a9f347d33624
DANIGAIN <danigain1234@gmail.com>
2023-05-05 21:46:07 +0600

    marge  Home

--- client/src/components/SortingVisualizer/SortingVisualizer.jsx
+++ client/src/components/SortingVisualizer/SortingVisualizer.jsx
@@ -1,39 +1,61 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
+import * as SortingAlgorithm from '../SortingAlgorithms/SortingAlgorithms';
 import './SortingVisualizer.css';
-import * as SortingAlgorithm from '../SortingAlgorithms/SortingAlgorithms'
 
 
-const NumberOfArrayBar = 50;
+const NumberOfArrayBar = 35;
 const animationSpeed = 100;
 
-
+//--------------------------------------- use case function ------------------------//
 
 let randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let arrayAreEqual = (arrayOne, arrayTwo) => {
-    if (arrayOne.lenght != arrayTwo.lenght) return false;
-    for (let i = 0; i < arrayOne.lenght; i++) {
-        if (arrayOne[i] != arrayTwo[i]) return false;
+// function waitforme() {
+//     return setTimeout(() => { resolve('') }, animationSpeed);
+// }
+
+
+let createNewArray = (array, setArray) => {
+    let a = [];
+
+    for (let i = 0; i < NumberOfArrayBar; i++) {
+        a.push(randomIntFromInterval(5, 700));
     }
-
-    return true;
+    setArray(a);
 }
-
-
+// ------------------------------------------- end use case function --------------------//
 function SortingVisualizer(props) {
+
+
     const [array, setArray] = useState([]);
     const [clickCount, setClickCount] = useState(1);
 
-    let resetArray = () => {
-        let a = [];
+    ///------------------------------------------  Implement algorithm  -------------------//
+    // function bubble() {
+    //     const element = document.querySelectorAll(".bar");
 
-        for (let i = 0; i < NumberOfArrayBar; i++) {
-            a.push(randomIntFromInterval(5, 730));
-        }
-        return a;
+    //     for(let i = 0 ;i<element.length-1;i++)
+    //     {
+    //         for(let j = 0 ; j<element.length-i-1 ; j++)
+    //         {
+    //           element[j].style.background = 'blue';
+    //           element[j+1].style.background = 'blue';
+    //           if(parseInt(element[j].style.height) >  parseInt(element[j+1].style.background))
+    //           {
+    //              waitforme();
+    //              swap(element[j] , element[j+1]);
+    //           }
+    //           element[j].style.background = 'cyan';
+    //           element[j+1].style.background = 'cyan';
 
-    }
+    //         }
+    //         element[element.length-i-1].style.background = 'green';
+    //     }
+
+    // }
+
+    //--------------------------end algorithm ----------------------------------//
 
     function mergeSort() {
 
@@ -62,20 +84,30 @@ function SortingVisualizer(props) {
         }
     }
 
+    function handleMergeSort()
+    {
+        mergeSort();
+    }
+    //----------------------------- call to sizebar  -------------------------//
 
+    if (props.clickCount === clickCount) {
 
-    if (props.clickCount === clickCount ) {
-      
         switch (props.clickEvent) {
             case "newArray":
                 {
-                    setArray(resetArray());
-                    setClickCount(prev => prev +1);
+                    createNewArray(array, setArray);
+                    setClickCount(prev => prev + 1);
                     break;
                 }
             case "marge":
                 {
-                    mergeSort()
+                    handleMergeSort()
+                    break;
+                }
+            case "bubble":
+                {
+
+                    console.log("ok");
                     break;
                 }
             default:
@@ -83,25 +115,36 @@ function SortingVisualizer(props) {
         }
     }
 
-
+    //-------------------------------------------------------------------------------//
     return (
-
         <>
-            <div className="array-container">
-                {array.map((value, index) =>
-                (
-                    <div
-                        className="array-bar"
-                        key={index}
-                        style={{ height: `${value}px` }}>
+            <div className="responsive">
+                <div className="array-container">
+                    {array.map((value, index) =>
+                    (
+                        <div
+                            className="array-bar"
+                            key={index}
+                            style={{ height: `${value}px` }}>
+                        </div>
+
+                    ))}
 
 
-                    </div>
-                ))}
-                <br />
+
+                    {/* <button onClick={() => setArray(resetArray)}> generate_new_array </button>
+            <button onClick={() => mergeSort()}> mergeSort </button>
+            <button onClick={() => bubbleSort()}> bubbleSort </button>
+            <button onClick={() => quickSort()}>quickSort </button>
+            <button onClick={() => heapSort()}> heapSort </button>
+            <button onClick={() => tasingArray()}> tasingArray </button> */}
+
+
+                </div>
             </div>
         </>
     )
+
 }
 
 export default SortingVisualizer;
