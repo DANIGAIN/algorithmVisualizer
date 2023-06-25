043a2243bbb7775d7dbc19ede32d949edf7f5a7e
dani <danigain1234@gmail.com>
2023-06-25 13:23:50 +0600

    solve linear search

--- src/components/SearchingVisualizer/SearchingVisualizer.jsx
+++ src/components/SearchingVisualizer/SearchingVisualizer.jsx
@@ -1,57 +1,63 @@
 import React, { useEffect, useState } from "react";
 
-import '../style.css';
+import "../style.css";
 
-export default function  SearchingVisualizer({newArray, newClick, speed})
-{
-    speed =  1000 - speed * 10 ;
-    const[searchElement, setSearchElement]=useState(10)
-    let flag = true;
-   
-    useEffect(()=>{
-        return ()=> {
-            setSearchElement(()=>prompt("Please Enter any number 0 to 500"))
-        }
-    }, [newClick=='binary', newClick=='linear'])
+export default function SearchingVisualizer({ newArray, newClick, speed }) {
+  speed = 1000 - speed * 10;
+  const [searchElement, setSearchElement] = useState(10);
 
-async function LinearSearch(){
-    const element = document.getElementsByClassName('array-bar');
+  useEffect(() => {
+    return () => {
+      setSearchElement(() => prompt("Please Enter any number 0 to 500"));
+    };
+  }, [newClick == "binary", newClick == "linear"]);
 
-    for(let i=0; i<element.length ; i++)
-    {
-        
-        element[i].style.backgroundColor = 'blue';
-        if(parseInt(element[i].style.height)===parseInt(searchElement))
-        {
-            element[i].style.backgroundColor = 'green';
-            flag=false;
-            i=element.length;
-        }
-        await new Promise((resolve) => setTimeout(resolve, speed));
-        
-        
+  async function LinearSearch() {
+    const element = document.getElementsByClassName("array-bar");
+    let flag = 0 ;
+
+    for (let i = 0; i < element.length; i++) {
+      element[i].style.backgroundColor = "blue";
+      if (parseInt(element[i].style.height) === parseInt(searchElement)) {
+        element[i].style.backgroundColor = "green";
+        flag = 1 ; 
+      }
+      else if(flag == 1)
+      {
+        element[i].style.backgroundColor = "black";
+      }
+      await new Promise((resolve) => setTimeout(resolve, speed));
     }
-}
-if(newClick=='linear')
-{
-    LinearSearch();
-    console.log(newArray);
-}
-//-------------------------------------------------------------------------------//
-    return (
-        <>
-         <div className="array-container">
-           {newArray.map((value, index) =>
-                    (
-                        <div
-                            className="array-bar"
-                            key={index}
-                            style={{ height: `${value}px` }}>
-                        </div>
+    setIndex(-1);
+  }
 
-                    ))}
-         </div>
-    
-        </>
-    )
+  useEffect(() => {
+    try {
+      if (newClick === "linear") {
+        LinearSearch().then(() => {
+          console.log(newArray);
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [newClick, newArray, searchElement, speed]);
+
+
+  //-------------------------------------------------------------------------------//
+  return (
+    <>
+      <div className="array-container">
+       { newArray.map((value, index) => (
+            <div
+            className="array-bar"
+            key={index}
+            style={{ height: `${value}px` }}
+          >
+            <span className="bar-value">{value}</span>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
