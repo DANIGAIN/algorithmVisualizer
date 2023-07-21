6ecfc71aa7478c8bfff7a46acccbbada583398cd
Jibon <jibonroy282@gmail.com>
2023-07-21 16:19:24 +0600

    modal

--- src/components/SearchingVisualizer/SearchingVisualizer.jsx
+++ src/components/SearchingVisualizer/SearchingVisualizer.jsx
@@ -5,53 +5,63 @@ import "../style.css";
 export default function SearchingVisualizer({ newArray, newClick, speed }) {
   speed = 1000 - speed * 10;
   const [searchElement, setSearchElement] = useState(10);
-
-  useEffect(() => {
-
-      if(newClick === 'linear' || newClick === 'binary'){
-        setSearchElement(() => prompt("Please Enter any number 0 to 500"));
-      }
-    
-  }, [newClick == "linear"]);
+  const [isModal, setIsModal] = useState(false);
 
   async function LinearSearch() {
     const element = document.getElementsByClassName("array-bar");
-    let flag = 0 ;
+    let flag = 0;
+console.log(searchElement);
+console.log(typeof(searchElement));
+    for (let i = 0; i < newArray.length; i++) {
+      console.log(newArray[i]);
+    //   element[i].style.backgroundColor = "blue";
+    //   if (parseInt(element[i].style.height) === parseInt(searchElement)) {
+    //     element[i].style.backgroundColor = "green";
+    //     flag = 1;
+    //   }
+    //   else if (flag == 1) {
+    //     element[i].style.backgroundColor = "black";
+    //   }
+    //   await new Promise((resolve) => setTimeout(resolve, speed));
+    // }
 
-    for (let i = 0; i < element.length; i++) {
-      element[i].style.backgroundColor = "blue";
-      if (parseInt(element[i].style.height) === parseInt(searchElement)) {
-        element[i].style.backgroundColor = "green";
-        flag = 1 ; 
-      }
-      else if(flag == 1)
-      {
-        element[i].style.backgroundColor = "black";
-      }
-      await new Promise((resolve) => setTimeout(resolve, speed));
-    }
 
   }
-
+  }
   useEffect(() => {
     try {
       if (newClick === "linear") {
+        setIsModal(true)
         LinearSearch().then(() => {
-          alert("not Found this List");
+          
         });
       }
     } catch (err) {
       console.log(err);
     }
-  }, [newClick, newArray, searchElement, speed]);
+  }, [newClick]);
+
+//---------------------------------Modal------------------------//
 
 
   //-------------------------------------------------------------------------------//
   return (
     <>
-      <div className="array-container">
-       { newArray.map((value, index) => (
-            <div
+
+      <div className={`modal2 ${!isModal?'hide':" "}`}>
+        
+
+          <div >
+            <button onClick={(e)=>{setIsModal(e.target.value)}}>X</button>
+            <label htmlFor="Row" className="text">Enter any number</label>
+            <input type="text" value={searchElement} onChange={(e)=>{setSearchElement(e.target.value)}} />
+          </div>
+        
+      </div>
+      <div className={`array-container ${isModal?`hide`:" "}`}>
+
+        {newArray.map((value, index) => (
+          <div
             className="array-bar"
             key={index}
             style={{ height: `${value}px` }}
