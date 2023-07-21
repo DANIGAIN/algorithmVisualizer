4478611c464bb0f18413c5eba3492370e8eebb0c
DANIGAIN <danigain1234@gmail.com>
2023-07-21 16:35:12 +0600

    change searchVisulizer

--- src/components/SearchingVisualizer/SearchingVisualizer.jsx
+++ src/components/SearchingVisualizer/SearchingVisualizer.jsx
@@ -4,46 +4,97 @@ import "../style.css";
 
 export default function SearchingVisualizer({ newArray, newClick, speed }) {
   speed = 1000 - speed * 10;
-  const [searchElement, setSearchElement] = useState(10);
+  const [searchElement, setSearchElement] = useState('');
+  //-------------- binary  -->
+  const [foundIndex, setFoundIndex] = useState(-1);
+  const [isSearching, setIsSearching] = useState(false);
 
-  useEffect(() => {
+  //------------------------------------------------------------ Linear Search -----------------------------------------------------------------//
 
-      if(newClick === 'linear' || newClick === 'binary'){
-        setSearchElement(() => prompt("Please Enter any number 0 to 500"));
-      }
-    
-  }, [newClick == "linear"]);
+ async function LinearSearch() {
+    //const element = document.getElementsByClassName("array-bar");
 
-  async function LinearSearch() {
-    const element = document.getElementsByClassName("array-bar");
-    let flag = 0 ;
 
-    for (let i = 0; i < element.length; i++) {
-      element[i].style.backgroundColor = "blue";
-      if (parseInt(element[i].style.height) === parseInt(searchElement)) {
-        element[i].style.backgroundColor = "green";
-        flag = 1 ; 
-      }
-      else if(flag == 1)
-      {
-        element[i].style.backgroundColor = "black";
-      }
-      await new Promise((resolve) => setTimeout(resolve, speed));
-    }
+    // if(!searchElement|| isNaN(searchElement)){
+    //   setSearchElement(prompt("Please Enter any number 0 to 500"));
+    // }
 
+
+    for (let i = 0; i < newArray.length; i++) {
+    //   if(foundIndex == -1) element[i].style.backgroundColor = "blue";
+    //   if (newArray[i] === parseInt(searchElement)) {
+    //     if(foundIndex == -1) element[i].style.backgroundColor = "green";
+    //     setFoundIndex(1);
+    //     return ;
+    //   }
+    //   else if(foundIndex == 1)
+    //   {
+    //     element[i].style.backgroundColor = "black";
+    //   }
+     await new Promise((resolve) => setTimeout(resolve, speed));
+    // }
+    console.log( newArray[i]);
   }
+}
 
-  useEffect(() => {
-    try {
-      if (newClick === "linear") {
-        LinearSearch().then(() => {
-          alert("not Found this List");
-        });
-      }
-    } catch (err) {
-      console.log(err);
+
+  //--------------------------------------------------------------- BInary search --------------------------------------------------------------//
+
+
+
+  const BinarySearch = () => {
+
+
+    console.log("binary");
+
+    if(!searchElement|| isNaN(searchElement)){
+      setSearchElement(() => prompt("Please Enter any number 0 to 500"));
+      return ;
     }
-  }, [newClick, newArray, searchElement, speed]);
+
+    setIsSearching(true);
+    let left = 0;
+    let right = newArray.length - 1;
+    let steps = [];
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      steps.push({ left, mid, right });
+
+      if (newArray[mid] === parseInt(searchElement)) {
+        setFoundIndex(mid);
+        setIsSearching(false);
+        return;
+      } else if (newArray[mid] < parseInt(searchElement)) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+     console.log(steps);
+    setIsSearching(false);
+    setFoundIndex(-1);
+  };
+
+
+
+  if(newClick === "linear") LinearSearch();
+
+  // useEffect(() => {
+  //   try {
+  //     console.log(newClick);
+  //     if (newClick === "linear") {
+  //       LinearSearch().then(() => {
+  //        // alert("not Found this List");
+  //       });
+  //     }
+  //     if(newArray == 'binary'){
+  //       BinarySearch().then(()=>{});
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }, [newClick]);
 
 
   //-------------------------------------------------------------------------------//
